a71afc6f138b69c244224e35d707b2526665d38b
@vishalsaxena29 <saxena.vishal204@gmail.com>
2023-02-12 14:19:34 +0530

    Script2-loops looping Intro

---
+++ shell-scripts/Script2-loops.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Lets take this to next level and start working on real things like loops
+
+#lets start with small lets create a for loop in shell script which prints number from 0 to 10 
+#Then we take this to something little big by printing odd,even,prime and then move to if then switch 
+#and then combine all.
+
+
+#1.print number 0 to 10 
+
+for(( i=0 ; i<=10 ; i++ ))
+do 
+echo $i
+echo ""
+done
