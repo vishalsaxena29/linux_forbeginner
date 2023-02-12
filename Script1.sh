c4998dc9716a0fee1a44429878c9236e1740f8e2
@vishalsaxena29 <saxena.vishal204@gmail.com>
2023-02-12 13:06:10 +0530

    Script1

--- Script1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read username  #Taking input from the user for the name which needes to be printed at the welcome line 
-echo "welcome $username" #This will print the welcome along with the string passed during read command.
-
-
-
-#This is your first and Basic Shell Script for starting your Scripting Journey. Happy Learning 
